1fea966899dd018d4af732218899e0d2db319b6a
Christopher Beards <BeardsCM@gmail.com>
2018-11-12 12:16:33 -0500

    db; converted connection strings to variables

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,11 @@
 <?php 
   // Database Connection
-  $connection = mysqli_connect('localhost', 'root', '', 'cms');
+  $db_host = 'localhost';
+  $db_user = 'root';
+  $db_pass = '';
+  $db_name = 'cms';
+  
+  $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
   if ($connection) {
     echo 'Connected to the DB!';
